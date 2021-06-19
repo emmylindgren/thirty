3d8fd0390ff7ee27385fe64720d9d60ea51a771e
Emmy Lindgren <id19eln@cs.umu.se>
2021-06-19 16:01:10 +0200

    Small fixes and cleaned up. Problem with rotation after play again-button is pressed

--- app/src/main/java/se/umu/emli/thirty/controller/MainActivity.java
+++ app/src/main/java/se/umu/emli/thirty/controller/MainActivity.java
@@ -48,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
         }
         else{
            setUpPrevious(savedInstanceState);
+           savedInstanceState.clear();
         }
 
         setUpDiceListeners();
